8da4ab6b52292c99c17980136fd6b199689a6ac3
Jason Carvalho <jase@jasoncarvalho.com>
2022-05-16 14:46:47 +0100

    Delete JSON documents

--- src/Controller/JSONBrowserController.php
+++ src/Controller/JSONBrowserController.php
@@ -31,12 +31,14 @@ class JSONBrowserController extends AbstractActionController
         $this->_permissionManager = $permissionManager;
     }
 
-    public function detailsAction()
+    public function writedocAction()
     {
         $user_id = $this->currentUser()->getId();
         $id = (int)$this->params()->fromRoute('id', 0);
         $dataset = $this->_dataset_repository->findDataset($id);
         //$permissions = $this->_repository->findDatasetPermissions($id);
+        $keyPassed = $this->params()->fromQuery('key', null); //KEY UUID passed on the query line
+        $docIDPassed = $this->params()->fromQuery('docid', null); //Doc ID passed on the query line
         $message = "Dataset: " . $id;
         $actions = [];
 
@@ -51,6 +53,75 @@ class JSONBrowserController extends AbstractActionController
             }
         }
 
+        $can_view = $this->_permissionManager->canView($dataset, $user_id);
+        $can_read = $this->_permissionManager->canRead($dataset, $user_id);
+        $can_edit = $this->_permissionManager->canEdit($dataset, $user_id);
+        $can_write = $this->_permissionManager->canWrite($dataset, $user_id);
+        $streamExists = $this->_repository->getStreamExists($dataset->uuid);
+        $keys = [];
+        //$userHasKey = false; //Does the user have a key on this stream (ie do they need to see all the API URLs)?
+        $userHasKey = $this->_keys_repository->userHasDatasetKey($user_id, $dataset->id);
+        $userDatasetKeys = $this->_keys_repository->userDatasetKeys($user_id, $dataset->id);
+
+        if ($can_view && $can_read && $can_write && $userHasKey) {
+            $keys = [];
+            if (!$streamExists) {
+                $this->flashMessenger()->addMessage('This dataset has not yet been activated.');
+                return $this->redirect()->toRoute('dataset', ['action' => 'details', 'id' => $id]);
+            }
+            $actions = [
+                'label' => 'Actions',
+                'class' => '',
+                'buttons' => [
+                ]
+            ];
+            $keys = $this->_keys_repository->userDatasetKeys($user_id,$dataset->id);
+            return new ViewModel([
+                'message' => $message,
+                'messages' => $messages,
+                'stream_exists' => $streamExists,
+                'keys' => $keys,
+                'dataset' => $dataset,
+                'stream_url' => $this->_repository->getApiReadHref($dataset->uuid),
+                'read_url' => $this->_repository->getApiReadHref($dataset->uuid),
+                'write_url' => $this->_repository->getApiWriteHref($dataset->uuid),
+                'browse_url' => $this->_repository->getApiBrowseHref($dataset->uuid),
+                'api_home' => $this->_repository->getApiHome(),
+                'features' => $this->datasetsFeatureManager()->getFeatures($id),
+                'actions' => $actions,
+                'can_edit' => $can_edit,
+                'can_read' => $can_read,
+                'can_write' => $can_write,
+                'user_has_key' => $userHasKey,
+                'userDatasetKeys' => $userDatasetKeys,
+                'key' => $keyPassed,
+                'docid' => $docIDPassed,
+            ]);
+        } else {
+            $this->flashMessenger()->addMessage('You do not have any suitable keys registered to this dataset for browsing JSON documents. Please use the API tab to register an access key.');
+            return $this->redirect()->toRoute('dataset', ['action' => 'details', 'id' => $id]);
+        }
+    }
+
+    public function detailsAction () {
+        $user_id = $this->currentUser()->getId();
+        $id = (int)$this->params()->fromRoute('id', 0);
+        $dataset = $this->_dataset_repository->findDataset($id);
+        //$permissions = $this->_repository->findDatasetPermissions($id);
+
+        $message = "Dataset: " . $id;
+        $actions = [];
+
+        $messages = [];
+        $flashMessenger = $this->flashMessenger();
+        if ($flashMessenger->hasMessages()) {
+            foreach ($flashMessenger->getMessages() as $flashMessage) {
+                $messages[] = [
+                    'type' => 'warning',
+                    'message' => $flashMessage
+                ];
+            }
+        }
         $can_view = $this->_permissionManager->canView($dataset, $user_id);
         $can_read = $this->_permissionManager->canRead($dataset, $user_id);
         $can_edit = $this->_permissionManager->canEdit($dataset, $user_id);
@@ -98,12 +169,81 @@ class JSONBrowserController extends AbstractActionController
                 'can_read' => $can_read,
                 'can_write' => $can_write,
                 'user_has_key' => $userHasKey,
-                'userDatasetKeys' => $userDatasetKeys,
+                'userDatasetKeys' => $userDatasetKeys
             ]);
         } else {
-            $this->flashMessenger()->addMessage('You do not have any suitable keys registered to this dataset for browsing JSON documents. Please use the API tab to register an access key.');
+            $this->flashMessenger()->addMessage('You do not have any suitable keys registered to this dataset for editing or creating JSON documents. Please use the API tab to register an access key.');
             return $this->redirect()->toRoute('dataset', ['action' => 'details', 'id' => $id]);
         }
+    }
+
+    public function deletedocAction () {
+        $user_id = $this->currentUser()->getId();
+        $id = (int) $this->params()->fromRoute('id', 0);
+        $dataset = $this->_dataset_repository->findDataset($id);
+        $keyPassed = $this->params()->fromQuery('key', null); //KEY UUID passed on the query line
+        $docIDPassed = $this->params()->fromQuery('docid', null); //Doc ID passed on the query line
+        $token = $this->params()->fromQuery('token', null);
+
+        $userDatasetKeys = $this->_keys_repository->userDatasetKeys($user_id,$dataset->id);
+        $permission = $this->userHasThisKeyOnDataset($keyPassed,$userDatasetKeys);
+        if (!$permission) {
+            $this->flashMessenger()->addMessage('Delete document failed: You do not have access to this dataset with this key.');
+            return $this->redirect()->toRoute('json', ['action'=>'details', 'id' => $id]);
+        }
+
+        if (ctype_upper($permission)) {
+            $this->flashMessenger()->addMessage('Delete document failed: This key has been disabled.');
+            return $this->redirect()->toRoute('stream', ['action'=>'details', 'id' => $id]);
+        }
+
+        if (is_null($token)) {
+            $token = uniqid(true);
+            $container = new Container('JSON_Delete_Doc');
+            $container->delete_token = $token;
+            $messages[] = [
+                'type'=> 'warning',
+                'message' => 'Are you sure you want to delete this document? This operation cannot be undone.'
+            ];
+            return new ViewModel(
+                [
+                    'dataset' => $dataset,
+                    'token' => $token,
+                    'key' => $keyPassed,
+                    'docid' => $docIDPassed,
+                    'messages' => $messages
+                ]
+            );
+        }
+        else {
+            $container = new Container('JSON_Delete_Doc');
+            $valid_token = ($container->delete_token == $token);
+            if ($valid_token) {
+                // Delete JSON document here...
+                $result = $this->_repository->deleteDocument($dataset->uuid,$docIDPassed,$keyPassed);
+                if ($result == "204") {
+                    $this->flashMessenger()->addMessage('Deleted document from dataset.');
+                }
+                else {
+                    $this->flashMessenger()->addMessage('Failed to delete document from dataset.');
+                }
+                //$this->_repository->removePermission($dataset->uuid, $keyPassed);
+                //$this->_keys_repository->removeKeyUUIDPermission($keyPassed, $id);
+
+                return $this->redirect()->toRoute('json', ['action'=>'details', 'id' => $id]);
+            }
+        }
+        return false;
+    }
+
+    private function userHasThisKeyOnDataset ($key,$userDatasetKeys) {
+        //print_r ($userDatasetKeys);
+        foreach ($userDatasetKeys as $datasetKey) {
+            if ($datasetKey['keyUUID'] == $key) {
+                return $datasetKey['permission'];
+            }
+        }
+        return false;
     }
 
 }
--- src/Repository/MKDFStreamRepository.php
+++ src/Repository/MKDFStreamRepository.php
@@ -104,6 +104,37 @@ class MKDFStreamRepository implements MKDFStreamRepositoryInterface
         $this->sendQuery("POST",$path, array('dataset-id'=>$uuid, 'read'=>$read, 'write'=>$write));
     }
 
+    public function getDocument ($dataset,$docId,$key) {
+        $username = $key;
+        $password = $key;
+        $server = $this->_config['mkdf-stream']['server-url'];
+        $path = '/object/'.$dataset.'/'.$docId;
+        $url = $server . $path;
+
+        $curl = curl_init();
+
+        $headers = array(
+            'Content-Type:application/json',
+            'Authorization: Basic '. base64_encode($username.":".$password) // <---
+        );
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => $headers,
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+
     public function getDocuments ($dataset,$numDocs,$key,$query = '{}') {
         //$username = $this->_config['mkdf-stream']['user'];
         //$password = $this->_config['mkdf-stream']['pass'];
@@ -138,6 +169,36 @@ class MKDFStreamRepository implements MKDFStreamRepositoryInterface
         $response = curl_exec($curl);
         curl_close($curl);
         return $response;
+    }
+
+    public function deleteDocument($dataset,$docID,$key) {
+        $username = $key;
+        $password = $key;
+        $server = $this->_config['mkdf-stream']['server-url'];
+        $path = '/object/'.$dataset.'/'.$docID;
+        $url = $server . $path;
+
+        $curl = curl_init();
+
+        $headers = array(
+            'Authorization: Basic '. base64_encode($username.":".$password)
+        );
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'DELETE',
+            CURLOPT_HTTPHEADER => $headers,
+        ));
+
+        $response = curl_exec($curl);
+        //curl_close($curl);
+        return curl_getinfo($curl,CURLINFO_RESPONSE_CODE);
     }
 
     public function pushDocument ($dataset,$document,$key=null) {
